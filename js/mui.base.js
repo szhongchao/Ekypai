19cf53f25d4a70d53709a9cf1e5d48c2c352d77d
szc <572233090@qq.com>
2017-05-20 14:48:41 +0800

    version 0.1

--- js/mui.base.js
+++ js/mui.base.js
@@ -1,2 +1,44 @@
 var basUrl = 'http://kypai.gotoip1.com/';
 //var basUrl = 'http://192.168.2.11:81/';
+function copyFromClip() {
+	var value = '';
+	switch(plus.os.name) {
+		case "Android":
+			// Android平台: plus.android.*
+			var Context = plus.android.importClass("android.content.Context");
+	        var main = plus.android.runtimeMainActivity();
+	        var clip = main.getSystemService(Context.CLIPBOARD_SERVICE);
+	        value = plus.android.invoke(clip,"getText");
+			break;
+		case "iOS":
+			// iOS平台: plus.ios.*
+			var UIPasteboard  = plus.ios.importClass("UIPasteboard");
+			var generalPasteboard = UIPasteboard.generalPasteboard();
+			// 获取文本内容:
+			value = generalPasteboard.valueForPasteboardType("public.utf8-plain-text");
+			break;
+		default:
+			break;
+	}
+	return value;
+}
+function copyToClip(uid) {
+	switch(plus.os.name) {
+		case "Android":
+			// Android平台: plus.android.*
+			var Context = plus.android.importClass("android.content.Context");
+			var main = plus.android.runtimeMainActivity();
+			var clip = main.getSystemService(Context.CLIPBOARD_SERVICE);
+			plus.android.invoke(clip, "setText", uid);
+			break;
+		case "iOS":
+			// iOS平台: plus.ios.*
+			var UIPasteboard  = plus.ios.importClass("UIPasteboard");
+			var generalPasteboard = UIPasteboard.generalPasteboard();
+			// 设置文本内容:
+			generalPasteboard.setValueforPasteboardType(uid, "public.utf8-plain-text");
+			break;
+		default:
+			break;
+	}
+}
